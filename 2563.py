5536d87b7d5bcd92849db80c43d88e4588e2ba10
Geonn <duagnlrjs@gmail.com>
2023-01-27 03:27:57 +0900

    refresh

--- 2563.py
+++ 2563.py
@@ -1,14 +1,19 @@
 #색종이 silver v
 
 #변수
-paper= [[0 for cor in range(100)] for row in range(100)]
-
+paper= [[0 for cor in range(101)] for row in range(101)]
+ans=0
 #입력
 for t in range(int(input())) :
     
     w,h = map(int,input().split())      #여백을 입력받음
     for i in range(10):
         for j in range(10):
-            paper[w+i][h+j]+=1
+            
+            paper[w+i][h+j]=1
+            
 for i in range(100) :
-    print(sum(paper[i]))
+    ans+=sum(paper[i])
+   
+    
+print(ans)
---
+++ 2750.py
@@ -0,0 +1,6 @@
+#수 정렬하기
+nums =[]
+for l in range(int(input())) :
+    nums.append(int(input()))
+for i in sorted(nums) :
+    print(i)
